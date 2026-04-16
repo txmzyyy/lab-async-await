958a948495f71e8da62b8a36c117aea8cc00bf98
txmzyyy <tamaramk08@gmail.com>
2026-04-16 10:28:23 +0300

    final code

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code here!
+const postList = document.getElementById('post-list');
+async function getPosts(){
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        if (!response.ok) {
+            throw new error("Failed to fetch posts")
+      }
+const posts = await response.json();
+
+displayPosts(posts);
+    } catch (error) {
+        console.error(error);
+    }
+}
+function displayPosts(posts){
+    posts.forEach(post => {
+        const postItem = document.createElement('li');
+
+        const h1 = document.createElement('h1');
+        h1.textContent = post.title;
+
+        const p = document.createElement('p');
+        p.textContent = post.body;
+
+        li.appendChild(h1);
+        li.appendChild(p);
+        postList.appendChild(li);
+    });
+}
+
+getPosts();
